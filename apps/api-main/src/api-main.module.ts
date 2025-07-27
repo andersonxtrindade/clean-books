f8ab3b73d761d801c7ba6a2f89aa8328897383f6
Anderson Trindade <axtrindade2012@yahoo.com>
2025-07-27 17:03:29 -0300

    chore: install and configure pino to loggers

--- apps/api-main/src/api-main.module.ts
+++ apps/api-main/src/api-main.module.ts
@@ -1,9 +1,24 @@
 import { Module } from '@nestjs/common';
 import { ApiMainController } from './api-main.controller';
 import { ApiMainService } from './api-main.service';
+import { LoggerModule } from 'nestjs-pino';
 
 @Module({
-  imports: [],
+  imports: [
+    LoggerModule.forRoot({
+      pinoHttp: {
+        transport:
+          process.env.NODE_ENV !== 'production'
+            ? {
+              target: 'pino-pretty',
+              options: {
+                singleLine: true,
+              }
+            }
+            : undefined,
+      }
+    })
+  ],
   controllers: [ApiMainController],
   providers: [ApiMainService],
 })
--- apps/api-main/src/main.ts
+++ apps/api-main/src/main.ts
@@ -1,8 +1,10 @@
 import { NestFactory } from '@nestjs/core';
 import { ApiMainModule } from './api-main.module';
+import { Logger } from 'nestjs-pino';
 
 async function bootstrap() {
-  const app = await NestFactory.create(ApiMainModule);
+  const app = await NestFactory.create(ApiMainModule, { bufferLogs: true });
+  app.useLogger(app.get(Logger));
   await app.listen(process.env.port ?? 3000);
 }
 bootstrap();
